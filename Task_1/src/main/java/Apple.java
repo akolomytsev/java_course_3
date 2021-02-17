2a27b822f76e67ccd3e3c202a78ca81a1d120b21
Kolomytsev Alexander <kolm.87@mail.ru>
2021-02-17 22:20:31 +0600

    task_1

---
+++ Task_1/src/main/java/Apple.java
@@ -0,0 +1,8 @@
+public class Apple extends Fruit {
+    private static final Float Apple_m1 = 1.0F; // добавил по видео
+
+    public Apple(){
+        super(Apple_m1); // Исправил по видео
+
+    }
+}
---
+++ Task_1/src/main/java/Orange.java
@@ -0,0 +1,10 @@
+public class Orange extends Fruit{
+    private static final Float Orange_m2 = 1.5F;
+
+
+    public Orange (){
+        super(Orange_m2);
+    }
+
+
+}
